4d0fbc367c24fe4669def085e850b1a62b1b7fa7
Ibrahim Aziz <ibraziz21@gmail.com>
2023-04-27 09:59:30 +0300

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -32,7 +32,8 @@ async function deployDiamond () {
   console.log('Deploying facets')
   const FacetNames = [
     'DiamondLoupeFacet',
-    'OwnershipFacet'
+    'OwnershipFacet',
+    'multisigFacet'
   ]
   const cut = []
   for (const FacetName of FacetNames) {
